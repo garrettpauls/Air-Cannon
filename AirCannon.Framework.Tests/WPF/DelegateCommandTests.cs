00a3840014f43fb3742ad039da14fb441b82b008
Garrett Pauls <garrett.pauls@gmail.com>
2011-03-03 19:52:42 -0700

    Added DelegateCommand and tests.

---
+++ AirCannon.Framework.Tests/WPF/DelegateCommandTests.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Windows.Input;
+using AirCannon.Framework.WPF;
+using MbUnit.Framework;
+
+namespace AirCannon.Framework.Tests.WPF
+{
+    /// <summary>
+    ///   Verifies that <see cref = "DelegateCommand" /> works correctly.
+    /// </summary>
+    [TestFixture]
+    public class DelegateCommandTests
+    {
+        protected bool mCanExecuteCalled = false;
+
+        protected bool mCanExecuteResult = true;
+        protected ICommand mCommand;
+        protected bool mExecuteCalled = false;
+
+        /// <summary>
+        ///   Verifies that CanExecute is called correctly.
+        /// </summary>
+        [Test]
+        public void CanExecuteTest()
+        {
+            mCanExecuteResult = true;
+            Assert.IsTrue(mCommand.CanExecute(null));
+            Assert.IsTrue(mCanExecuteCalled);
+
+            mCanExecuteResult = false;
+            Assert.IsFalse(mCommand.CanExecute(null));
+            Assert.IsTrue(mCanExecuteCalled);
+        }
+
+        /// <summary>
+        ///   Verifies the Execute delegate is only called if CanExecute is true.
+        /// </summary>
+        [Test]
+        public void ExecuteTest()
+        {
+            mCanExecuteResult = true;
+            mCommand.Execute(null);
+            Assert.IsTrue(mExecuteCalled);
+
+            mCanExecuteResult = false;
+            mExecuteCalled = false;
+            mCommand.Execute(null);
+            Assert.IsFalse(mExecuteCalled);
+        }
+
+        /// <summary>
+        ///   Resets the command.
+        /// </summary>
+        [SetUp]
+        public void SetUp()
+        {
+            mCommand = CreateCommand();
+            mCanExecuteCalled = false;
+            mExecuteCalled = false;
+            mCanExecuteResult = true;
+        }
+
+        protected bool CanExecute()
+        {
+            mCanExecuteCalled = true;
+            return mCanExecuteResult;
+        }
+
+        protected virtual ICommand CreateCommand()
+        {
+            return new DelegateCommand(Execute, CanExecute);
+        }
+
+        protected void Execute()
+        {
+            mExecuteCalled = true;
+        }
+    }
+
+    [TestFixture]
+    public class GenericDelegateCommandTests : DelegateCommandTests
+    {
+        private int? mCanExecuteCalledWith;
+        private int? mExecuteCalledWith;
+
+        /// <summary>
+        ///   Verifies that CanExecute gets called with the right parameter.
+        /// </summary>
+        [Test]
+        public void CanExecuteParameterTest()
+        {
+            const int PARAMETER = -4;
+
+            mCanExecuteResult = true;
+            mCanExecuteCalledWith = 0;
+            Assert.IsTrue(mCommand.CanExecute(PARAMETER));
+            Assert.IsTrue(mCanExecuteCalled);
+            Assert.AreEqual(PARAMETER, mCanExecuteCalledWith);
+
+            mCanExecuteResult = false;
+            mCanExecuteCalledWith = 0;
+            Assert.IsFalse(mCommand.CanExecute(PARAMETER));
+            Assert.IsTrue(mCanExecuteCalled);
+            Assert.AreEqual(PARAMETER, mCanExecuteCalledWith);
+        }
+
+        /// <summary>
+        ///   Verifies that calling CanExecute with a parameter of the wrong type causes an
+        ///   InvalidCastException to be thrown.
+        /// </summary>
+        [Test, ExpectedException(typeof (InvalidCastException))]
+        public void CanExecuteWrongParameterTypeTest()
+        {
+            mCommand.CanExecute("Blah");
+        }
+
+        /// <summary>
+        ///   Verifies that Execute gets called with the correct parameter.
+        /// </summary>
+        [Test]
+        public void ExecuteParameterTest()
+        {
+            const int PARAMETER = 9;
+            mCanExecuteResult = true;
+            mExecuteCalledWith = 0;
+            mCommand.Execute(PARAMETER);
+            Assert.IsTrue(mExecuteCalled);
+            Assert.AreEqual(PARAMETER, mExecuteCalledWith);
+
+            mCanExecuteResult = false;
+            mExecuteCalled = false;
+            mExecuteCalledWith = 0;
+            mCommand.Execute(PARAMETER);
+            Assert.IsFalse(mExecuteCalled);
+            Assert.AreEqual(0, mExecuteCalledWith);
+        }
+
+        /// <summary>
+        ///   Verifies that calling Execute with a parameter of the wrong type causes an
+        ///   InvalidCastException to be thrown.
+        /// </summary>
+        [Test, ExpectedException(typeof (InvalidCastException))]
+        public void ExecuteWrongParameterTypeTest()
+        {
+            mCommand.Execute("Blah");
+        }
+
+        protected override ICommand CreateCommand()
+        {
+            return new DelegateCommand<int?>(_Execute, _CanExecute);
+        }
+
+        private bool _CanExecute(int? parameter)
+        {
+            mCanExecuteCalledWith = parameter;
+            return CanExecute();
+        }
+
+        private void _Execute(int? parameter)
+        {
+            mExecuteCalledWith = parameter;
+            Execute();
+        }
+    }
+}
---
+++ AirCannon.Framework/WPF/DelegateCommand.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AirCannon.Framework.WPF
+{
+    /// <summary>
+    ///   An implementation of <see cref = "ICommand" /> which uses delegates to define functionality.
+    /// </summary>
+    public class DelegateCommand : DelegateCommand<object>
+    {
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "DelegateCommand" /> class.
+        /// </summary>
+        /// <param name = "execute">The method to call when the command is executed.</param>
+        /// <param name = "canExecute">
+        ///   The method that determines if the command can execute. 
+        ///   If <c>null</c>, the command can always be executed.
+        /// </param>
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+            : base(obj => execute(), canExecute != null
+                                         ? new Func<object, bool>(obj => canExecute())
+                                         : null)
+        {
+        }
+
+        /// <summary>
+        ///   Determines if the command can execute.
+        /// </summary>
+        public bool CanExecute()
+        {
+            return CanExecute(null);
+        }
+
+        /// <summary>
+        ///   Determines if the command can execute.
+        /// </summary>
+        public void Execute()
+        {
+            Execute(null);
+        }
+    }
+
+    /// <summary>
+    ///   An implementation of <see cref = "ICommand" /> which uses delegates to define functionality.
+    /// </summary>
+    /// <typeparam name = "TParameter">The type of the parameter.</typeparam>
+    public class DelegateCommand<TParameter> : ICommand
+    {
+        private readonly Func<TParameter, bool> mCanExecute;
+        private readonly Action<TParameter> mExecute;
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "DelegateCommand&lt;TParameter&gt;" /> class.
+        /// </summary>
+        /// <param name = "execute">The method to call when the command is executed.</param>
+        /// <param name = "canExecute">
+        ///   The method that determines if the command can execute. 
+        ///   If <c>null</c>, the command can always be executed.
+        /// </param>
+        public DelegateCommand(Action<TParameter> execute, Func<TParameter, bool> canExecute = null)
+        {
+            mExecute = execute;
+            mCanExecute = canExecute;
+        }
+
+        #region ICommand Members
+
+        /// <summary>
+        ///   Determines if the command can execute.
+        /// </summary>
+        /// <param name = "parameter">
+        ///   Data used by the command. It should be of type <typeparamref name = "TParameter" />.
+        /// </param>
+        /// <returns>
+        ///   true if this command can be executed; otherwise, false.
+        /// </returns>
+        public bool CanExecute(object parameter)
+        {
+            return CanExecute((TParameter) parameter);
+        }
+
+        /// <summary>
+        ///   Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        ///   Executes the command.
+        /// </summary>
+        /// <param name = "parameter">
+        ///   Data used by the command. It should be of type <typeparamref name = "TParameter" />.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            Execute((TParameter) parameter);
+        }
+
+        #endregion
+
+        /// <summary>
+        ///   Determines if the command can execute.
+        /// </summary>
+        /// <param name = "parameter">
+        ///   Data used by the command.
+        /// </param>
+        /// <returns>
+        ///   true if this command can be executed; otherwise, false.
+        /// </returns>
+        public bool CanExecute(TParameter parameter)
+        {
+            if (mCanExecute == null)
+            {
+                return true;
+            }
+            return mCanExecute(parameter);
+        }
+
+        /// <summary>
+        ///   Executes the command.
+        /// </summary>
+        /// <param name = "parameter">
+        ///   Data used by the command.
+        /// </param>
+        public void Execute(TParameter parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                mExecute(parameter);
+            }
+        }
+
+        /// <summary>
+        ///   Raises the <see cref = "CanExecuteChanged" /> event.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            var temp = CanExecuteChanged;
+            if (temp != null)
+            {
+                temp(this, EventArgs.Empty);
+            }
+        }
+    }
+}
